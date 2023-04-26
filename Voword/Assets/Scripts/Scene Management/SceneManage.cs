938a51006690bb6bdf814f410eec6fc12c0e4ef9
Reimash <91878793+Reimash@users.noreply.github.com>
2023-04-26 23:32:30 +0800

    level 1 complete fully

---
+++ Voword/Assets/Scripts/Scene Management/SceneManage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public void Load1point1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Load1point2()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Load1point3()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Load1point4()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void Load1point5()
+    {
+        SceneManager.LoadScene(15);
+    }
+}
