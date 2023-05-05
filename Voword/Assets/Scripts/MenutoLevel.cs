e4c86ce583cdfb61bb3ca679279c33c8713cf15c
Reimash <91878793+Reimash@users.noreply.github.com>
2023-05-05 14:51:59 +0800

    all level

--- Voword/Assets/Scripts/MenutoLevel.cs
+++ Voword/Assets/Scripts/MenutoLevel.cs
@@ -17,11 +17,25 @@ public class MenutoLevel : MonoBehaviour
 
     public void LoadLevelTwo()
     {
+<<<<<<< Updated upstream:Voword/Assets/Scripts/MenutoLevel.cs
         SceneManager.LoadScene(3);
     }
 
     public void LoadLevelThree()
     {
         SceneManager.LoadScene(4);
+=======
+        SceneManager.LoadScene(16);
+    }
+
+    public void LoadLevelTwoFreeRoam()
+    {
+        SceneManager.LoadScene(19);
+    }
+
+    public void LoadLevelThreeFreeRoam()
+    {
+        SceneManager.LoadScene(34);
+>>>>>>> Stashed changes:Voword/Assets/Scripts/Scene Management/MenutoLevel.cs
     }
 }
---
+++ Voword/Assets/Scripts/Scene Management/SceneManage.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManage : MonoBehaviour
+{
+    public void Load1point1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Load1point2()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Load1point3()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void Load1point4()
+    {
+        SceneManager.LoadScene(12);
+    }
+
+    public void Load1point5()
+    {
+        SceneManager.LoadScene(15);
+    }
+
+    public void Loadtutorialkill()
+    {
+        SceneManager.LoadScene(18);
+    }
+
+    public void Load2point1()
+    {
+        SceneManager.LoadScene(21);
+    }
+
+    public void Load2point2()
+    {
+        SceneManager.LoadScene(24);
+    }
+
+    public void Load2point3()
+    {
+        SceneManager.LoadScene(27);
+    }
+
+    public void Load2point4()
+    {
+        SceneManager.LoadScene(30);
+    }
+
+    public void Load2point5()
+    {
+        SceneManager.LoadScene(33);
+    }
+
+    public void Load3point1()
+    {
+        SceneManager.LoadScene(36);
+    }
+
+    public void Load3point2()
+    {
+        SceneManager.LoadScene(39);
+    }
+
+    public void Load3point3()
+    {
+        SceneManager.LoadScene(42);
+    }
+
+    public void Load3point4()
+    {
+        SceneManager.LoadScene(45);
+    }
+
+    public void Load3point5()
+    {
+        SceneManager.LoadScene(48);
+    }
+}
+
